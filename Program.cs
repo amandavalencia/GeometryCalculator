34169ef947a632091060183568041dd14ba8ba68
amandavalencia <amandaavalencia@hotmail.com>
2025-09-27 13:07:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CircleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle firstCircle = new Circle(5);
+            Circle secondCircle = new Circle(6);
+
+            var areaOfFirstCircle = firstCircle.GetArea();
+            var areaOfSecondCircle = secondCircle.GetArea();
+
+            Console.WriteLine($"Arean av första cirkeln: {areaOfFirstCircle}");
+            Console.WriteLine($"Arean av andra cirkeln: {areaOfSecondCircle}");
+
+        }
+    }
+}
